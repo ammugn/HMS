b6c6735b9e91fa0ab37a8e3a9c55b58526d1b187
Ammu Nair <ammugn@gmail.com>
2022-06-27 14:01:18 -0700

    Commit before adding detailed security features.Added dependencies for Spring security

--- src/main/java/com/perscholas/hms/controllers/HomeController.java
+++ src/main/java/com/perscholas/hms/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.perscholas.hms.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @author Ammu Nair
  */
 @Controller
+@Slf4j
 public class HomeController {
 
     @GetMapping(value = {"/", "index"})
@@ -14,4 +16,11 @@ public class HomeController {
 
         return "dashboard";
     }
+    @GetMapping(value = {"login"})
+    public String loginPage(){
+        log.info("In security");
+        log.info("Test");
+        return "index";
+    }
+
 }
